247878a9a424aa324767112811ef18479cd2b6bc
sanjayPlus <sanjaysanthoshplusitpark@gmail.com>
2024-03-05 15:17:45 +0530

    first commit

---
+++ config/SERVER_URL.js
@@ -0,0 +1,2 @@
+export default "https://dcc-kozhikode.onrender.com/api";
+// export default "https://dccbackend.plusitpark.com/api";
---
+++ contants/districtLeaders.js
@@ -0,0 +1,68 @@
+const districtLeaders = [
+    {
+        id: 1,
+        name: "T N Prathapan",
+        position: "Member of Lok Sabha",
+        image: "/images/national-leaders/prathapan.png",
+        facebook: "https://www.facebook.com/kalaiselvam",
+        instagram: "https://www.instagram.com/kalaiselvam/",
+        youtube: "https://www.youtube.com/channel/UCp8nXqQJ9nQn7X9FJ3fU9Xg"
+    },
+    {
+        id: 2,
+        name: "Jose Vallur",
+        position: "DCC President",
+        image: "/images/national-leaders/jose.png",
+        facebook: "https://www.facebook.com/kalaiselvam",
+        instagram: "https://www.instagram.com/kalaiselvam/",
+        youtube: "https://www.youtube.com/channel/UCp8nXqQJ9nQn7X9FJ3fU9Xg"
+    },
+    {
+        id: 3,
+        name: "M P Vincent",
+        position: "Member of Kerala Legislative Assembly",
+        image: "/images/national-leaders/vincent.jpg",
+        facebook: "https://www.facebook.com/kalaiselvam",
+        instagram: "https://www.instagram.com/kalaiselvam/",
+        youtube: "https://www.youtube.com/channel/UCp8nXqQJ9nQn7X9FJ3fU9Xg"
+    },
+    {
+        id: 4,
+        name: "Sundaran Kunnathully",
+        position: "INTUC President - Thrissur",
+        image: "/images/national-leaders/sundaran.png",
+        facebook: "https://www.facebook.com/kalaiselvam",
+        instagram: "https://www.instagram.com/kalaiselvam/",
+        youtube: "https://www.youtube.com/channel/UCp8nXqQJ9nQn7X9FJ3fU9Xg"
+    },
+    {
+        id: 5,
+        name: "Anil Akkara",
+        position: "Indian politician of India National Congress",
+        image: "/images/national-leaders/anil.jpg",
+        facebook: "https://www.facebook.com/kalaiselvam",
+        instagram: "https://www.instagram.com/kalaiselvam/",
+        youtube: "https://www.youtube.com/channel/UCp8nXqQJ9nQn7X9FJ3fU9Xg"
+    },
+    {
+        id: 6,
+        name: "T J Saneesh Kumar Joseph",
+        position: "Member of Kerala Legislative Assembly",
+        image: "/images/saneesh.jpeg",
+        facebook: "https://www.facebook.com/kalaiselvam",
+        instagram: "https://www.instagram.com/kalaiselvam/",
+        youtube: "https://www.youtube.com/channel/UCp8nXqQJ9nQn7X9FJ3fU9Xg"
+    },
+    {
+        id: 7,
+        name: "P A Madhavan",
+        position: "Former Member of Kerala Legislative Assembly",
+        image: "/images/national-leaders/madhavan.jpg",
+        facebook: "https://www.facebook.com/kalaiselvam",
+        instagram: "https://www.instagram.com/kalaiselvam/",
+        youtube: "https://www.youtube.com/channel/UCp8nXqQJ9nQn7X9FJ3fU9Xg"
+    }
+    
+]
+
+export default districtLeaders;
